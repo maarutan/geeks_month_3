626945b8c52036153d3a409049c54d23c3e5fc6b
maaru <maratarzymatov288@gmail.com>
2024-11-06 15:39:30 +0600

    это 1 домашка

--- work_1/main.py
+++ work_1/main.py
@@ -1,10 +1,24 @@
-from config import bot, dp
+from decouple import Config, RepositoryEnv  # type: ignore
 from aiogram import Bot, Dispatcher, executor, types  # type: ignore
 import logging
-from handlers import command, quiz
 
-command.register_commands(dp)
-quiz.register_quiz(dp)
+
+config = Config(RepositoryEnv("../.env"))
+token = config("TOKEN2")
+bot = Bot(token=token)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=["start"])
+async def start(message: types.Message):
+    await bot.send_message(
+        chat_id=message.from_user.id,
+        text=f"Hello! {message.from_user.first_name}\n"
+        f"your Telegram id = {message.from_user.id}",
+    )
+
+    await message.answer("Hello! I'm a bot")
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
